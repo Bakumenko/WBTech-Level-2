7940afb1f772ca3c455ca752a5dc59d0026e9b26
Oleg <74712214+obakumen@users.noreply.github.com>
2021-07-26 19:36:50 +0300

    :zap:Basic task 1 realization.

---
+++ develop/basic/go.mod
@@ -0,0 +1,8 @@
+module basic
+
+go 1.16
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
+)
--- develop/basic/main.go
+++ develop/basic/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
 
+func main() {
+	options := ntp.QueryOptions{Timeout: 30 * time.Second, TTL: 10}
+	response, err := ntp.QueryWithOptions("0.beevik-ntp.pool.ntp.org", options)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	err = response.Validate()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	time := time.Now().Add(response.ClockOffset).Format("15:04:05")
+	fmt.Fprintln(os.Stderr, time)
+	os.Exit(1)
 }
