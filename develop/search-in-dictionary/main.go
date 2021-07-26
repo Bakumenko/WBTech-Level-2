ce0e79570059147b99d27a8755f8b3c93ef23855
Oleg <74712214+obakumen@users.noreply.github.com>
2021-07-26 19:38:05 +0300

    :zap:Search in dictionary task 4 realization.

--- develop/search-in-dictionary/main.go
+++ develop/search-in-dictionary/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	slice := []string{
+		"пятак",
+		"листок",
+		"олег",
+		"пятка",
+		"тяпка",
+		"ЛЕГО",
+		"слиток",
+		"мукА",
+		"столик",
+		"акума",
+	}
+
+	result := SearchAnagramByDictionary(&slice)
+	fmt.Println(*result)
 }
