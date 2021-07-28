7d335a6f7423642fec4d521fdd426edb5fd81d8a
Oleg <74712214+obakumen@users.noreply.github.com>
2021-07-29 02:33:23 +0300

    :white_check_mark:Change old tests and add a new test for new feature.

--- develop/unpacking/unpacking_test.go
+++ develop/unpacking/unpacking_test.go
@@ -18,12 +18,16 @@ func TestUnpackingString(t *testing.T) {
 		{`qwe\4\5`, "qwe45", nil},
 		{`qwe\45`, "qwe44444", nil},
 		{`qwe\\5`, `qwe\\\\\`, nil},
+		{`q\413`, "q4444444444444", nil},
+		{"a0b2c12", "bbcccccccccccc", nil},
+		{`qw\\\e`, "", errors.New("invalid string")},
 	}
 	for _, table := range tables {
 		totalS, totalErr := UnpackingString(table.s)
-		if totalS != table.res || totalErr != table.err {
-			t.Errorf("UnpackingString of (%v) was incorrect, got: %v, want: %v with error: %v, wanted error: %v\n",
-				table.s, table.res, totalS, table.err, totalErr)
+
+		if totalS != table.res || totalErr == nil && table.err != nil || totalErr != nil && table.err == nil {
+			t.Errorf("UnpackingString of (%v) was incorrect, got: (%v), want: (%v) with error: (%v), wanted error: (%v)\n",
+				table.s, totalS, table.res, totalErr, table.err)
 		}
 	}
 }
