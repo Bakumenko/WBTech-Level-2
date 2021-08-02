d66ccfa4a7dfc9c7cccf2417b8d822b94ec442f9
Oleg <74712214+obakumen@users.noreply.github.com>
2021-08-02 12:02:47 +0300

    :zap:Update middleware. Add new layer for server.

--- develop/rest/cmd/main.go
+++ develop/rest/cmd/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"simplerest/pkg/handler"
 	"simplerest/pkg/repository"
-	"time"
+	"simplerest/pkg/server"
 )
 
 func main() {
@@ -25,44 +25,15 @@ func run() error {
 	repos := repository.NewRepository(db)
 	hndlr := handler.NewHandler(repos)
 	router := http.NewServeMux()
+	server := server.InitServer(hndlr, router)
 
 	addr := "127.0.0.1:" + webPort
 	logrus.WithField("addr", addr).Info("starting server")
 
-	err = startServer(router, hndlr)
+	err = server.StartServer(webPort)
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func startServer(router *http.ServeMux, h *handler.Handler) error {
-	router.HandleFunc("/events_for_day", h.GetEventsForDay)
-	router.HandleFunc("/events_for_week", h.GetEventsForWeek)
-	router.HandleFunc("/events_for_month", h.GetEventsForMonth)
-	router.HandleFunc("/create_event", h.CreateEvent)
-	router.HandleFunc("/update_event", h.UpdateEvent)
-	router.HandleFunc("/delete_event", h.DeleteEvent)
-	configuredRouter := LoggingMiddleware(router)
-	return http.ListenAndServe(":"+webPort, configuredRouter)
-}
-
-func LoggingMiddleware(h http.Handler) http.Handler {
-	logFn := func(rw http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		uri := r.RequestURI
-		method := r.Method
-		duration := time.Since(start)
-
-		logrus.WithFields(logrus.Fields{
-			"uri":      uri,
-			"method":   method,
-			"duration": duration,
-		}).Info("input request")
-
-		h.ServeHTTP(rw, r)
-	}
-	return http.HandlerFunc(logFn)
 }
