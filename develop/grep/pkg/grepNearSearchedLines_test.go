ddcc7092fe02d2288401fe18bdc634bba541a726
Oleg Bakumenko <Bakumenko.Oleg@wildberries.ru>
2021-08-04 16:46:44 +0300

    :white_check_mark: Add test for grep task 4.

--- develop/grep/pkg/grepNearSearchedLines_test.go
+++ develop/grep/pkg/grepNearSearchedLines_test.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -18,31 +19,33 @@ func Test_grepLinesNearSearched(t *testing.T) {
 		resLines     []string
 		resCount     int
 	}{
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, false, false, false, []string{"asd"}, 0},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, false, false, false, []string{"oleg", "ert", "rty", "QWE"}, 2},
 
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, false, false, false, []string{"asd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, true, false, false, []string{"asd", "aSd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, false, true, false, []string{"1. asd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, false, false, true, []string{"1. asd"}, 0},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, false, false, false, []string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, 3},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, true, false, false, []string{"1. oleg", "2. ert", "3. rty", "4. QWE"}, 2},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, false, true, false, []string{"oleg", "ert", "rty", "QWE"}, 2},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, false, false, true, []string{"ert", "rty", "QWE", "asd", "zxc"}, 4},
 
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, true, false, false, []string{"asd", "aSd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, false, true, false, []string{"1. asd", "2. aSd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, false, false, true, []string{"1. asd", "2. aSdd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, true, true, false, []string{"asd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, true, false, true, []string{"asd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, false, true, true, []string{"asd"}, 0},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, true, false, false, []string{"1. oleg", "2. ert", "3. rty", "4. QWE", "5. asd", "6. zxc"}, 3},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, false, true, false, []string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, 3},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, false, false, true, []string{"ert", "rty", "QWE", "asd", "zxc"}, 3},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, true, true, false, []string{"1. oleg", "2. ert", "3. rty", "4. QWE"}, 2},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, true, false, true, []string{"2. ert", "3. rty", "4. QWE", "5. asd", "6. zxc"}, 4},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, false, true, true, []string{"ert", "rty", "QWE", "asd", "zxc"}, 4},
 
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, true, true, false, []string{"asd", "aSd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, true, false, true, []string{"1. asd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, false, true, true, []string{"1. asd"}, 0},
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, false, true, true, true, []string{"asd", "aSd"}, 0},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, true, true, false, []string{"1. oleg", "2. ert", "3. rty", "4. QWE", "5. asd", "6. zxc"}, 3},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, true, false, true, []string{"2. ert", "3. rty", "4. QWE", "5. asd", "6. zxc"}, 3},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, false, true, true, []string{"ert", "rty", "QWE", "asd", "zxc"}, 3},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, false, true, true, true, []string{"2. ert", "3. rty", "4. QWE", "5. asd", "6. zxc"}, 4},
 
-		{[]string{"asd", "aSd", "qwe"}, "asd", 0, 0, true, true, true, true, []string{"1. asd", "2. aSd"}, 0},
+		{[]string{"oleg", "ert", "rty", "QWE", "asd", "zxc"}, "e", 1, 2, true, true, true, true, []string{"2. ert", "3. rty", "4. QWE", "5. asd", "6. zxc"}, 3},
 	}
 	for _, table := range tables {
 		resultLines, resultCount, _ := grepLinesNearSearched(table.textByLines, table.targerString, table.before, table.after, table.ignoreCase, table.lineNumber, table.regular, table.invert)
 
-		assert.Equal(t, resultLines, table.resLines)
-		assert.Equal(t, resultCount, table.resCount)
+		inputData := fmt.Sprintf("lines = (%v)\ntarget = (%v)\ncount before = %v, count after = %v\nignore case = %v, print line = %v, use regular = %v, invert = %v",
+			table.textByLines, table.targerString, table.before, table.after, table.ignoreCase, table.lineNumber, table.regular, table.invert)
+		assert.Equal(t, resultLines, table.resLines, inputData)
+		assert.Equal(t, resultCount, table.resCount, inputData)
 	}
 }
